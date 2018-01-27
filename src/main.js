a9af4d9f205683ce47c221eadae26d8f1780efbd
Fantasy <805841483@qq.com>
2018-01-28 01:09:48 +0800

    add:添加初始配置

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import axios from 'axios'
 
 Vue.use(ElementUI)
+Vue.prototype.$http = axios
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
 })
